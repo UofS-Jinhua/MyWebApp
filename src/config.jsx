150292308a86f0a58f4e4c6a874557cc05081c46
Ubuntu <HUA@MyNoteVM.j030kp4pz2kejbzmodlkyotwvh.ux.internal.cloudapp.net>
2024-12-19 21:14:03 +0000

    change backend url

--- src/config.jsx
+++ src/config.jsx
@@ -1,6 +1,7 @@
 const config = {
   // apiBaseUrl: "http://20.175.207.79:3000",
-  apiBaseUrl: "http://localhost:3000",
+  // apiBaseUrl: "http://20.63.89.37:3000",
+  apiBaseUrl: "http://127.0.0.1:3000",
 };
 
 export default config;
